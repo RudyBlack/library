cfee580a978aae476f1a2bb6f43ad48e9a9fc133
LudyBlack <test@example.com>
2020-08-01 19:11:48 +0900

    테스트용 함수 추가

--- dev/doTest.js
+++ dev/doTest.js
@@ -12,7 +12,26 @@ document.body.innerHTML =
 </span>`;
 
 
-describe('getUrlFileName 함수 테스트',function(){
+
+
+describe('sliceString 함수 테스트',function(){
+	it("sliceString은 리턴값이 배열이다.", function () {
+			expect(  Array.isArray( TestUtil.sliceString("1",1) ) ).to.equal( true );
+	});
+	it("sliceString('ababab',2) == ['ab','ab','ab']", function () {
+			expect( TestUtil.sliceString('ababab',2) ).to.eql(['ab','ab','ab'] );
+	});
+	it("sliceString('abcbcbdddggg',3) == ['abc','bcb','ddd','ggg']", function () {
+			expect( TestUtil.sliceString('abcbcbdddggg',3) ).to.eql( ['abc','bcb','ddd','ggg'] );
+	});
+	it("sliceString('남현우입니다',6) == ['남현우입니다']", function () {
+			expect( TestUtil.sliceString('남현우입니다',6) ).to.eql( ['남현우입니다'] );
+	});
+	it("sliceString(78787979,0) 에러뱉음", function () {
+			expect( TestUtil.sliceString(78787979,3) ).to.eql( error );
+	});
+});
+describe.skip('getUrlFileName 함수 테스트',function(){
 	it("https://devlive.club5678.com/r_mgnf/dsfdsf/aaa.html은 aaa와 같다", function () {
 			expect( TestUtil.getUrlFileName("https://devlive.club5678.com/r_mgnf/dsfdsf/aaa.html") ).to.equal( "aaa" );
 	});
--- dev/testUtil.js
+++ dev/testUtil.js
@@ -17,3 +17,24 @@ export let getUrlFileName = function(url){
   var urlSplit = url.split("/");
   return urlSplit[ urlSplit.length - 1 ].split(".")[0];
 }
+
+export function sliceString(target, count){
+  if(typeof target !== 'string') throw '첫번째 매개변수는 string이여야합니다.';
+  let rtnVal = [];
+  let beforeCount = 0;
+  let nextCount = count;
+  if (count === 0) {
+   rtnVal.push(target);
+   return rtnVal;
+  }
+  for (var i = 0; i < target.length; i++) {
+   if (nextCount >= target.length) {
+    rtnVal.push(target.slice(beforeCount, target.length));
+    break;
+   }
+   rtnVal.push(target.slice(beforeCount, nextCount));
+   beforeCount = nextCount;
+   nextCount = beforeCount + count;
+  };
+  return rtnVal;
+}
