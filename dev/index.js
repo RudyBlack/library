b352acf610ee2461be332ea449b98b69cf4e9ff6
rudyBlack <skadudgh1341@gmail.com>
2020-10-12 12:29:56 +0000

    [Doing work]test code Simplify
    For get easily function name in test code.

--- dev/index.js
+++ dev/index.js
@@ -18,60 +18,55 @@ const should = chai.should();
 document.body.innerHTML = '<span id="target" class="target"><span class="fkfk"></span></span>';
 
 /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
+const test = {
+  domUtil: {
+    makeDomElement: () => {
+      expect(domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' })).to.not.have.property('type');
+      expect(domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' })).to.not.have.property('valude');
+      expect(domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' })).to.have.property('className');
+      expect(domUtil.makeDomElement({ elType: 'input', className: 'hello', type: 'button' })).to.have.property('type');
+      expect(domUtil.makeDomElement({ elType: 'input', className: 'hello' })).to.have.property('className');
+      expect(domUtil.makeDomElement({ elType: 'input', className: 'hello', bgColor: 'blue' })).to.not.have.property('bgColor');
+      expect(domUtil.makeDomElement({ elType: 'input', className: 'hello' })).to.have.property('value');
+      expect(domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' })).to.not.have.property('is');
+    },
+  },
+  httpUtil: {},
+  dataUtil: {
+    StateManagement: () => {
+      const stateManagement = dataUtil.StateManagement;
+      stateManagement.set('hello', 1);
+      stateManagement.set('hello', 2);
+      stateManagement.set('Array', []);
+      stateManagement.set('Array2', [1, 2, 3, 4, 5]);
 
-describe.skip('arrayUtil', () => {});
+      expect(stateManagement.get('hello')).to.equal(2);
+      expect(stateManagement.get('Array')).to.have.lengthOf(0);
+      expect(stateManagement.get('Array2')).to.have.lengthOf(5);
+    },
+  },
+  stringUtil: {
+    sliceString: () => {
+      expect(stringUtil.sliceString('hello', 1)).to.have.lengthOf(5);
+      expect(stringUtil.sliceString('hello', 4)).to.have.lengthOf(2);
+      expect(stringUtil.sliceString('hello', 6)).to.have.lengthOf(1);
+      expect(stringUtil.sliceString('hello', 0)).to.have.lengthOf(1);
+      expect(stringUtil.sliceString('[]', 2)).to.eql(['[]']);
+    },
+    compressString: () => {
 
-describe('domUtil', () => {
-  it('makeDomElement', () => {
-    expect(
-      domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' }),
-    ).to.not.have.property('type');
-    expect(
-      domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' }),
-    ).to.not.have.property('valude');
-    expect(
-      domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' }),
-    ).to.have.property('className');
-    expect(
-      domUtil.makeDomElement({ elType: 'input', className: 'hello', type: 'button' }),
-    ).to.have.property('type');
-    expect(domUtil.makeDomElement({ elType: 'input', className: 'hello' })).to.have.property(
-      'className',
-    );
-    expect(
-      domUtil.makeDomElement({ elType: 'input', className: 'hello', bgColor: 'blue' }),
-    ).to.not.have.property('bgColor');
-    expect(domUtil.makeDomElement({ elType: 'input', className: 'hello' })).to.have.property(
-      'value',
-    );
-    expect(
-      domUtil.makeDomElement({ elType: 'div', className: 'hello', type: 'button' }),
-    ).to.not.have.property('is');
+    },
+  },
+};
+
+for (const key in test) {
+  describe(key, () => {
+    for (const method in test[key]) {
+      console.log(method);
+
+      it(method, () => {
+        test[key][method]();
+      });
+    }
   });
-});
-
-describe('httpUtil', () => {});
-
-describe('dataUtil', () => {
-  it('StateManagement', () => {
-    const stateManagement = dataUtil.StateManagement;
-    stateManagement.set('hello', 1);
-    stateManagement.set('hello', 2);
-    stateManagement.set('Array', []);
-    stateManagement.set('Array2', [1, 2, 3, 4, 5]);
-
-    expect(stateManagement.get('hello')).to.equal(2);
-    expect(stateManagement.get('Array')).to.have.lengthOf(0);
-    expect(stateManagement.get('Array2')).to.have.lengthOf(5);
-  });
-});
-
-describe('stringUtil', () => {
-  it('sliceString', () => {
-    expect(stringUtil.sliceString('hello', 1)).to.have.lengthOf(5);
-    expect(stringUtil.sliceString('hello', 4)).to.have.lengthOf(2);
-    expect(stringUtil.sliceString('hello', 6)).to.have.lengthOf(1);
-    expect(stringUtil.sliceString('hello', 0)).to.have.lengthOf(1);
-    expect(stringUtil.sliceString('[]', 2)).to.eql(['[]']);
-  });
-});
+}
