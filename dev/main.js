3e8ae82fd94b733704cccfcfc434d4561aa84fbc
rudyBlack <skadudgh1341@gmail.com>
2020-11-02 07:15:20 +0000

    clean up

--- dev/main.js
+++
@@ -1,7 +0,0 @@
-import * as arrayUtil from './testCode/array.js';
-import * as dataUtil from './testCode/data.js';
-import * as domUtil from './testCode/dom.js';
-import * as httpUtil from './testCode/http.js';
-import * as stringUtil from './testCode/string.js';
-import * as assetString from './testCode/asset/assetString.js';
-
