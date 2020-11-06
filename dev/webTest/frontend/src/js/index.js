92f5463b26ec813e6c873432e84ab4b82c6629c4
rudyBlack <skadudgh1341@gmail.com>
2020-11-06 03:37:53 +0000

    Add Chrome console test

--- dev/webTest/frontend/src/js/index.js
+++ dev/webTest/frontend/src/js/index.js
@@ -1,1 +1,4 @@
-import { arrayUtil, dataUtil, domUtil, httpUtil, stringUtil } from '/testCode/index.js';
+import * as testCode from '/testCode/index.js';
+
+//For test in chrom console..
+window.testCode = testCode;
